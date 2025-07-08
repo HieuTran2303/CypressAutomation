12199bdebcbc9321de99b6e43f449e4a44fa0638
HieuTran <zorrotran2303@gmail.com>
2025-07-08 23:24:20 +0700

    update

---
+++ cypress/e2e/HomePageTesting.cy.js
@@ -0,0 +1,62 @@
+import HomePage from "../pageObjects/homePage";
+
+describe('Elements Testing', () => {
+  it('should be clickable elements menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.elementsMenuSelector()).click(); // Click on the Elements menu
+    cy.log('Elements menu clicked successfully'); // Log message to console
+  });
+});
+
+describe('Forms Testing', () => {
+  it('should be clickable forms menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.formsMenuSelector()).click(); // Click on the Forms menu
+    cy.log('Forms menu clicked successfully'); // Log message to console
+  });
+});
+
+describe('Alerts, Frame & Windows Testing', () => {
+  it('should be clickable alerts, frame & windows menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.alertsFrameWindowsMenuSelector()).click(); // Click on the Alerts, Frame & Windows menu
+    cy.log('Alerts, Frame & Windows menu clicked successfully'); // Log message to console
+  });
+});
+
+describe('Widgets Testing', () => {
+  it('should be clickable widgets menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.widgetsMenuSelector()).click(); // Click on the Widgets menu
+    cy.log('Widgets menu clicked successfully'); // Log message to console
+  });
+});
+
+describe('Interactions Testing', () => {
+  it('should be clickable interactions menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.interactionsMenuSelector()).click(); // Click on the Interactions menu
+    cy.log('Interactions menu clicked successfully'); // Log message to console
+  });
+});
+
+describe('Book Store Application Testing', () => {
+  it('should be clickable book store application menu', () => {
+    cy.visit('https://demoqa.com/');  // URL test
+    const homePage = new HomePage();
+    cy.get(homePage.bookStoreApplicationMenuSelector()).click(); // Click on the Book Store Application menu
+    cy.log('Book Store Application menu clicked successfully'); // Log message to console
+  });
+});
+// End of file: cypress/e2e/HomePageTesting.cy.js
+// This file contains tests for various menu items on the home page of the demo website.  
+// Each test case visits the demo website and clicks on a specific menu item, logging a success message to the console.
+// The tests cover the Elements, Forms, Alerts, Frame & Windows, Widgets, Interactions, and Book Store Application menus.
+// The HomePage class is used to encapsulate the selectors for each menu item, making the tests more maintainable and readable.
+// The tests are structured using Cypress's `describe` and `it` functions, allowing for clear organization and reporting of test results.
+// The `cy.visit` command is used to navigate to the demo website before each test, ensuring a consistent starting point. 
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -1,15 +1,36 @@
 class HomePage {
 
-  static elementsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(1) h531'; // Selector for the Elements menu
-
-  // Method to get the Elements menu selector
-  elementsMenuSelector() {
-    return HomePage.elementsMenuSelector;
-  }
+  // Selectors for various menu items on the home page
+  elementsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(1) h5'; // Selector for the Elements menu
+  formsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2) h5'; // Selector for the Forms menu
+  alertsFrameWindowsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(3) h5'; // Selector for the Alerts, Frame & Windows menu
+  widgetsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(4) h5'; // Selector for the Widgets menu
+  interactionsMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(5) h5'; // Selector for the Interactions menu
+  bookStoreApplicationMenuSelector = 'body>div:nth-child(7)>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div:nth-child(1)>div:nth-child(6) h5'; // Selector for the Book Store Application menu
 
   // Method to click on the Elements menu
   elementsMenuClick() {
-    cy.get(HomePage.elementsMenuSelector).click(); // Click on the Elements menu
+    cy.get(this.elementsMenuSelector).click(); // Click on the Elements menu
+  }
+  // Method to click on the Forms menu
+  formsMenuClick() {
+    cy.get(this.formsMenuSelector).click(); // Click on the Forms menu
+  }
+  // Method to click on the Alerts, Frame & Windows menu
+  alertsFrameWindowsMenuClick() {
+    cy.get(this.alertsFrameWindowsMenuSelector).click(); // Click on the Alerts, Frame & Windows menu
+  }
+  // Method to click on the Widgets menu
+  widgetsMenuClick() {
+    cy.get(this.widgetsMenuSelector).click(); // Click on the Widgets menu
+  }
+  // Method to click on the Interactions menu
+  interactionsMenuClick() {
+    cy.get(this.interactionsMenuSelector).click(); // Click on the Interactions menu
+  }
+  // Method to click on the Book Store Application menu
+  bookStoreApplicationMenuClick() {
+    cy.get(this.bookStoreApplicationMenuSelector).click(); // Click on the Book Store Application menu
   }
 }
 export default HomePage;
